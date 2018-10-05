9c65b7475b84c1cace9479c4deaa67558a9dd4e1
epam <epam@local.rsu.edu.ru>
2018-10-05 21:15:58 +0300

    test

--- src/ex2_js-basics-part2/task3.js
+++ src/ex2_js-basics-part2/task3.js
@@ -1,5 +1,19 @@
-var array=[1, 8, 2 , 6];
+var array=[1, 8, 2 , 6 ,0];
 var i=0;
+var a=0;
+var b=0;
+var k=0;
 var f=function(array){
-    if()
+    while(i<array.length){
+    if(array[i]%2===1){
+        ++b;
+    } else if(array[i]%2===0){
+        ++a   ; 
+    } else if(array[i]===0){
+        ++k;    
+    }
+    ++i    
+    }
+    console.log([a,b,k])
 }
+f(array)
