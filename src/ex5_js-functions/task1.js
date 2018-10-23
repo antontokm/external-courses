8f9bc44e762150f700207cc548df3033166d0dc2
antontokm <ant.tokmakov@gmail.com>
2018-10-23 17:50:06 +0300

    Calculator

--- src/ex5_js-functions/task1.js
+++ src/ex5_js-functions/task1.js
@@ -1,55 +1,83 @@
-// var Calculator={
-//     add:function(a,b){
-//             x=a+b;
-//             console.log(x);
-//     },
-//     subtract:function(a,b){
-//             x=a-b;
-//             console.log(x);
-//     },
-//     divide:function(a,b){
-//             x=a/b;
-//             console.log(x);
-//     },    
-//     multiply:function(a,b){
-//             x=a*b;
-//             console.log(x);
-//     },
-//     getResult:function(){
-//         console.log(x)
-//     },
-//     reset: function(){
-//         x=0;
-//         console.log("reset");
-//     }        
-// }
-
-// console.log(Calculator.add(1,5));
-// console.log(Calculator.getResult());
-// console.log(Calculator.reset());
-var x =0;
-var Calculator={
-    getResult:function(){
-        console.log(x);
-    },
-    add:function(a){
-        x=x+a;
-        console.log(x);
-    },
-    subtract:function(a){
-        x=x-a;
-        cosnole.log(x);
-    },
-    divide:function(a){
-        x=x/a;
-        console.log(x);
-    },
-    multiply:function(a){
-        x=x*a;
-        console.log(x);
+var makeCalculator= function(){
+    var result=0;
+    return{
+        // add:function(a){
+        //     var self = this;
+        //     if(arguments.length==0){
+        //          return self.getResult;
+        //     }else{
+        //         result=result+a;
+        //         return function(b){
+        //             if(arguments.length==0){
+        //                 return self.getResult;
+        //             }else{
+        //                 result+=b;
+        //                 return self.add;
+        //             }    
+        //         }
+        //     }       
+        // },
+        add:function(a){
+            if(arguments.length != 0){
+                result = result + a;
+            }function myCallBack (b){
+                if(arguments.length != 0){
+                    result = result + b;
+                }
+                return myCallBack
+            }
+            return myCallBack
+        },
+        subtract:function(a){
+            if(arguments.length != 0){
+                result = result - a;
+            }function myCallBack (b){
+                if(arguments.length != 0){
+                    result = result - b;
+                }
+                return myCallBack
+            }
+            return myCallBack   
+        },
+        divide:function(a){
+            if(arguments.length != 0){
+                result = result / a;
+            }function myCallBack (b){
+                if(arguments.length != 0){
+                    result = result / b;
+                }
+                return myCallBack
+            }
+            return myCallBack
+        },
+        multiply:function(a){
+            if(arguments.length != 0){
+                result = result * a;
+            }function myCallBack (b){
+                if(arguments.length != 0){
+                    result = result * b;
+                }
+                return myCallBack
+            }
+            return myCallBack    
+        },
+        getResult:function(){
+            return result;
+        },
+        reset:function(){
+            return result=0;
+        }
     }
 }
-console.log(Calculator.add(5));
-console.log(Calculator.divide(2.5))
-// console.log(Calculator.getResult());
-// console.log(Calculator.reset());
+var Calculator = makeCalculator();
+Calculator.add(2)(4)(10)(2);
+console.log(Calculator.getResult());
+Calculator.multiply(2);
+console.log(Calculator.getResult());
+Calculator.subtract(2)(4)(10)(2);
+console.log(Calculator.getResult());
+Calculator.divide(9)(2);
+console.log(Calculator.getResult());
+Calculator.subtract(1)(2)()()(4)()()(1);
+console.log(Calculator.getResult());
+console.log(Calculator.reset());
